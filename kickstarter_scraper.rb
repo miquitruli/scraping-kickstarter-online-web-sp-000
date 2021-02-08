b03bc2b9c3ca25cf42c218eb0a03f0c499f9fcaf
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2021-02-08 23:09:09 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,14 +1,12 @@
-# file: kickstarter_scraper.rb
-
-require 'nokogiri'
-require 'pry'
+require "nokogiri"
+require "pry"
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
- 
+
 create_project_hash
 
 
